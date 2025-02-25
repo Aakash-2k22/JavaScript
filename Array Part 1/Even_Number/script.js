3ffbd27cda432324eecd91efa4308d9dc3a9fc5f
Aakash Keshari <aakashkeshari27@gmail.com>
2025-02-26 00:57:57 +0530

    Add files via upload

---
+++ Array Part 1/Even_Number/script.js
@@ -0,0 +1,7 @@
+let arr = [1,2,2,5,4,8,6,7,9,5,3,2,4,6,5,7,2];
+
+for(let i=0 ; i<arr.length ; i++){
+    if(arr[i]%2==0){
+        document.write(arr[i] + " ");
+    }
+}
---
+++ Array Part 1/Even_Odd/script.js
@@ -0,0 +1,9 @@
+let arr = [1,2,2,5,4,8,6,7,9,5,3,2,4,6,5,7,2];
+
+for(let i=0 ; i<arr.length ; i++){
+    if(arr[i]%2==0){
+        document.write("0 ");
+    }else{
+        document.write("1 ");
+    }
+}
